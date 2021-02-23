9acd971374e5fd92ba0c5cd1ed9a7420690ce355
patrickmurray73 <g00344530@gmit.ie>
2021-02-23 19:16:33 +0000

    Added basic UI to landing page

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import '../styles/landing.css';
+import Button from 'react-bootstrap/Button';
 
 export class Landing extends React.Component {
     render() {
         return(
-            <div>
-                <h1>Landing Page</h1>
+            <div className="landing">
+                <h1>Cryptolio</h1>
+                <br></br><br></br><br></br>
+                <p>
+                    Enter Introduction for Cryptolio here.
+                </p>
+                <br></br>
+                <Button variant="success">Login</Button>
+                <br></br><br></br>
+                <Button variant="outline-success">Register</Button>
             </div>
         );
     }

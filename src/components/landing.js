68c5421c4a24ef1e31fd20da02ab984e7d12b56b
PatrickMurray78 <g00344530@gmit.ie>
2021-02-24 23:41:46 +0000

    Updated Landing Page UI

--- src/components/landing.js
+++ src/components/landing.js
@@ -2,20 +2,30 @@ import React from 'react';
 import '../styles/landing.css';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import logo from '../images/logo.png';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 
 export class Landing extends React.Component {
     render() {
         return(
             <div className="landing">
-                <h1>Cryptolio</h1>
-                <br></br><br></br><br></br>
-                <p>
-                    Enter Introduction for Cryptolio here.
-                </p>
-                <br></br>
-                <Link to="/login" className="btn btn-success btn-lg">Login</Link>
-                <br></br><br></br>
-                <Link to="/register" className="btn btn-outline-success btn-lg">Register</Link>
+                <Navbar id="navbar" width="100%">
+                    <img
+                        src={logo}
+                        width="50"
+                        height="50"
+                        className="d-inline-block align-top"
+                    />
+                    <h1 id="brand">Cryptolio</h1>
+                    <Nav.Link as={Link} to="/login" className="login btn btn-success ml-auto">Login</Nav.Link>
+                </Navbar>
+                <br></br><br></br><br></br><br></br><br></br>
+                <h1>
+                    Track all your Crypto in the one place from <br></br>anywhere in the world.
+                </h1>
+                <Link to="/register" className="register btn btn-outline-success">Register</Link>
             </div>
         );
     }

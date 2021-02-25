01e7a3eb13ce13daa0a6cd21778d4cc02db8cf07
patrickmurray73 <g00344530@gmit.ie>
2021-02-25 22:33:37 +0000

    Created Popup component

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from '../images/logo.png';
 import '../styles/portfolio.css';
 import { Cryptos } from './cryptos';
+import Link from 'react-router-dom/Link';
+//import Popup from 'reactjs-popup';
+//import 'reactjs-popup/dist/index.css';
+import Popup from './Popup';
+
 
 export class Portfolio extends React.Component {
 
@@ -48,6 +53,15 @@ export class Portfolio extends React.Component {
                     </tr>
                 </table>
                 <Cryptos cryptos={this.state.cryptos}></Cryptos>
+                <Link to={'/create'}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16" style={{position: "absolute", bottom: "20px", color: "white"}}>
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                    </svg>
+                </Link>
+                <Popup trigger={true}>
+                    <h3>My Popup</h3>
+                </Popup>
             </div>
         );
     }

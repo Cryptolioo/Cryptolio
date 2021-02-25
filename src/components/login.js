828f3859a7d4c8e343c878e871bd16d76016486b
Cory <coryodonoghue1@gmail.com>
2021-02-25 14:07:00 +0000

    Added text fields for register

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {Button, Form , FormGroup, Input , Label } from "reactstrap";
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../images/logo.png';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 export class Login extends React.Component {
     render() {
         return(
+
             <Form className="login-form">
-                <h1>
-                    <span className = "font-weight-bold">Cryptolioo</span>
-                </h1>
+                 <Navbar id="loginNavbar" width="100%">
+                    <img
+                        src={logo}
+                        width="50"
+                        height="50"
+                        className="d-inline-block align-top"
+                    />
+                    <h1 id="brand">Cryptolio</h1>
+                </Navbar>
                 <h3>
                     <span className = "font-weight-bold">Welcome Back!</span>
                 </h3>
@@ -23,6 +34,7 @@ export class Login extends React.Component {
 
                 <Button className = "btn-lg btn-dark btn-block">Log in</Button>
  
+             {/* allow link to bring back to register page or forgot password */}
               <div className = "text-center">
               <a href = "/sign-up"> Sign up</a>
               <span className = "p-2">|</span>
--- src/components/register.js
+++ src/components/register.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import {Button, Form , FormGroup, Input , Label } from "reactstrap";
+import logo from '../images/logo.png';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
 
 export class Register extends React.Component {
     render() {
         return(
-            <div className="register">
-                <h1>Cryptolio</h1>
-            </div>
+              <Form  className="register-form">
+                
+                 <FormGroup>
+                    <Label className="fname" >First Name</Label>
+                    <Input type="fname" placeholder="First Name"/>
+                </FormGroup>
+
+                <FormGroup>
+                    <Label className ="sname" >Last Name</Label>
+                    <Input type="sname" placeholder="Last Name"/>
+                </FormGroup>
+
+                <FormGroup>
+                    <Label className ="email">Email</Label>
+                    <Input type = "email" placeholder = "Email"/>
+                </FormGroup>
+
+                <FormGroup>
+                    <Label className="password">Password</Label>
+                    <Input type="password" placeholder = "password"/>
+                </FormGroup>
+
+               
+              </Form>
         );
     }
 }

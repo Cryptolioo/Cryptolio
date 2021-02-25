be8392aafc7a22c5a02fd3372339c85f7a1c539e
PatrickMurray78 <g00344530@gmit.ie>
2021-02-25 20:03:31 +0000

    Create table for Crypto.js

--- src/components/crypto.js
+++ src/components/crypto.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import '../styles/crypto.css';
 
 export class Crypto extends React.Component {
     render() {
-        
+        return (
+            <table id="crypto_table">
+                <tr>
+                    <td width="8%">
+                        <h5>{this.props.crypto.Ticker}</h5>
+                    </td>
+                    <td width="24%">
+                        <h5>${this.props.crypto.Price}</h5>
+                    </td>
+                    <td width="10%">
+                        <h5>{this.props.crypto.Holdings}</h5>
+                    </td>
+                    <td width="25%">
+                    </td>
+                </tr>
+            </table>
+        )
     }
 }

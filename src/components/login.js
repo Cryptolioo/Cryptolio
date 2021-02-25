f3493ac2a28e44f2df49710039d9b480b0e63dfe
Cory <coryodonoghue1@gmail.com>
2021-02-25 12:09:30 +0000

    Added password textfield

--- src/components/login.js
+++ src/components/login.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import {Button, Form , FormGroup, Input , Label } from "reactstrap";
 
 export class Login extends React.Component {
     render() {
         return(
-            <form>
-                <h3>Login</h3>
+            <Form className="login-form">
+                <h1>
+                    <span className = "font-weight-bold">Cryptolioo</span>
+                </h1>
+                <FormGroup>
+                    <Label className="email" >Email</Label>
+                    <Input type="email" placeholder="Email"/>
+                </FormGroup>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-                
-            </form>        
+                <FormGroup>
+                    <Label className ="password" >Password</Label>
+                    <Input type="password" placeholder="Password"/>
+                </FormGroup>
+
+            </Form>
+    
+         
         );
     }
 }

e90ec015724072e60f1cf38a42eb0eac5fb7c467
PatrickMurray78 <g00344530@gmit.ie>
2021-02-25 20:46:33 +0000

    Added get request to /api/cryptos

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/api/cryptos', (req, res) => {
+  const mycryptos = [
+    {
+      "Ticker": "BTC",
+      "Price": "54000",
+      "Holdings": "34"
+    },
+    {
+      "Ticker": "ETH",
+      "Price": "1700",
+      "Holdings": "127"
+    }
+  ];
+  res.json({cryptos:mycryptos})
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })

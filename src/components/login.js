2ca9da34465d33684e2956d333af2de140ac4ef1
Cory <coryodonoghue1@gmail.com>
2021-02-25 12:11:48 +0000

    Added log in button, sign up and forgot password links

--- src/components/login.js
+++ src/components/login.js
@@ -18,6 +18,14 @@ export class Login extends React.Component {
                     <Input type="password" placeholder="Password"/>
                 </FormGroup>
 
+                <Button className = "btn-lg btn-dark btn-block">Log in</Button>
+ 
+              <div className = "text-center">
+              <a href = "/sign-up"> Sign up</a>
+              <span className = "p-2">|</span>
+              <a href = "/forgot-password">Forgot Password</a>
+              </div>
+
             </Form>
     
          

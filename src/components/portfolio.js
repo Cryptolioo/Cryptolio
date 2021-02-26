4587900bff9975cdc1866347962c9c9fcb9dbdd4
patrickmurray73 <g00344530@gmit.ie>
2021-02-26 14:33:46 +0000

    Added UI for create popup

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../images/logo.png';
 import '../styles/portfolio.css';
+import '../styles/create.css';
 import { Cryptos } from './cryptos';
 import Link from 'react-router-dom/Link';
 //import Popup from 'reactjs-popup';
@@ -61,7 +62,32 @@ function Portfolio() {
                     </svg>
                 </Link>
                 <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-                    <h3>My Popup</h3>
+                    <h2 id="create_header">Add Crypto</h2>
+                    <form>
+                        <div className="form-group">
+                            <label>Add Ticker: </label>
+                            <input type='text'
+                            className='form-control'></input>
+                        </div>
+                        <div className="form-group">
+                            <label>Add Holdings: </label>
+                            <input type='text'
+                            name='holdings'
+                            className='form-control'></input>
+                        </div>
+                        <div className="confirm-cancel">
+                            <Link onClick={() => setButtonPopup(false)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16"  fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16" style={{color: "white"}}>
+                                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+                                </svg>
+                            </Link>
+                            <Link onClick={() => setButtonPopup(false)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"  fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16" style={{color: "white"}}>
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </Link>
+                        </div>
+                    </form>
                 </Popup>
             </div>
         );

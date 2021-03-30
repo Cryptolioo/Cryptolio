98e980e7aee37ad30bdd93d2f19964951843f6f5
Cory <coryodonoghue1@gmail.com>
2021-03-30 14:48:36 +0100

    Fixed register merge conflicts

--- src/components/register.js
+++ src/components/register.js
@@ -4,7 +4,6 @@ import logo from '../images/logo.png';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
-<<<<<<< HEAD
 import axios from 'axios';
 
 export class Register extends React.Component {
@@ -129,58 +128,6 @@ export class Register extends React.Component {
                         </div>
 
                     </div>
-=======
-
-export class Register extends React.Component {
-
-    render() {
-        return (
-            <Form className="register-form" >
-                <Navbar id="loginNavbar" width="100%">
-                    <img
-                        src={logo}
-                        width="50"
-                        height="50"
-                        className="d-inline-block align-top"
-                    />
-                    <h1 id="brand">Cryptolio</h1>
-                </Navbar>
-
-                <div className="paddingBox">
-
-                    <FormGroup>
-                        <Label className="fname" >First Name</Label>
-                        <Input type="text" placeholder="First Name">
-                        </Input>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label className="sname" >Last Name</Label>
-                        <Input type="text" placeholder="Last Name"></Input>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label className="email">Email</Label>
-                        <Input type="email" placeholder="Email"></Input>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Label className="password">Password</Label>
-                        <Input type="password" placeholder="password"></Input>
-                    </FormGroup>
-
-                    {/* <Nav.Link as={Link} to="/login" className="btn-lg btn-dark btn-block" onClick={this.onSubmit}>Register</Nav.Link> */}
-
-
-                    {/* add in if on  */}
-                    <Button className="btn-lg btn-dark btn-block" > Register</Button>
-                    <div className="text-center">
-                        <p> Already have an account?<a href="/forgot-password">Log in</a></p>
-                        
-                    </div>
-
-                </div>
->>>>>>> login
             </Form>
         );
     }

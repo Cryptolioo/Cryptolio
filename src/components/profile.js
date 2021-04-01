4f8acf88c7cf5092bfb1bab40c2aab7482f43de7
patrickmurray73 <g00344530@gmit.ie>
2021-04-01 14:46:53 +0100

    Added profile component and stylesheet

---
+++ src/components/profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/profile.css';
+import { Link } from 'react-router-dom';
+import logo from '../images/logo.png';
+
+
+export class Profile extends React.Component {
+    render() {
+        return(
+        );
+    }
+}

d499f462ec5b59d159f5a676814ee20f70749a2c
patrickmurray73 <g00344530@gmit.ie>
2021-04-02 20:16:09 +0100

    Fixed add crypto icon

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -83,7 +83,7 @@ export class Portfolio extends React.Component {
                 </table>
                 <Cryptos cryptos={this.state.cryptos} ReloadData={this.ReloadData}></Cryptos>
                 <Link onClick={() => this.addCrypto(true)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16" style={{position: "absolute", bottom: "20px", color: "white", zIndex: "1"}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16" className="add-crypto">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                     </svg>

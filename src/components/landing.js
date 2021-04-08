3123ae5f5b100bee75c907bdd639b5184e9bde78
patrickmurray73 <g00344530@gmit.ie>
2021-04-08 14:38:38 +0100

    Added nav to calc/contact at landing.js

--- src/components/landing.js
+++ src/components/landing.js
@@ -10,7 +10,7 @@ import Nav from 'react-bootstrap/Nav';
 export class Landing extends React.Component {
     render() {
         return(
-            <div className="landing">
+            <div className="landing ">
                 <Navbar id="navbar" width="100%">
                     <img
                         src={logo}
@@ -19,8 +19,11 @@ export class Landing extends React.Component {
                         className="d-inline-block align-top"
                     />
                     <h1 id="brand">Cryptolio</h1>
+                    <Nav.Link as={Link} to="/calculator" className="calc-link ml-auto">Calculator</Nav.Link>
+                    <Nav.Link as={Link} to="/contact-us" className="contact-link">Contact Us</Nav.Link>
                     <Nav.Link as={Link} to="/login" className="login btn btn-success ml-auto">Login</Nav.Link>
                 </Navbar>
+                
                 <h1 id="intro">
                     Track all your Crypto in the one place from <br></br>anywhere in the world.
                 </h1>
